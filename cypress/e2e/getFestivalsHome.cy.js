1d128175940d2dcde738b1d19722083b81455983
alokayasassri <aloka.yasassri001@gmail.com>
2022-08-01 17:37:54 +1000

    Update getFestivalsHome.cy.js

--- cypress/e2e/getFestivalsHome.cy.js
+++ cypress/e2e/getFestivalsHome.cy.js
@@ -1,38 +1,34 @@
 
- /// <reference types="cypress"/>
+/// <reference types="cypress"/>
 
 describe('Test 1 : Festivals Web App Validation ', () => {
- 
- beforeEach(function(){
 
-  cy.visit('http://localhost:4200/festivals')
- })
+  beforeEach(function () {
+
+    cy.visit('http://localhost:4200/festivals')
+  })
 
 
   it('Test 2 : Verify Festivals Home page load', () => {
-   
-  
     cy.log("Web Page successfully loaded")
-  
   })
 
   it('Test 3 : Verify Trainerella Festival availability on Home page', () => {
-   
-    if(cy.get('html').contains('Trainerella'))
-    { 
+
+    if (cy.get('html').contains('Trainerella')) {
       cy.log("Trainerella Festival is available on the Home Page")
     }
-   
+
   })
 
   it('Test 4 : Verify Festivals list', () => {
-    
+
     cy.contains('LOL-palooza')
     cy.contains('Small Night In')
     cy.contains('Twisted Tour')
-    cy.contains('Trainerella') 
+    cy.contains('Trainerella')
     cy.log("All Festivals are available on the Home Page")
-    
+
   })
 
   it('Test 5 : Verify Bands list', () => {
